5e893cbdd5abc111084ec3bbea8e780f7ddfe244
JMarshall5 <31601677+JMarshall5@users.noreply.github.com>
2024-08-19 13:57:48 -0700

    Level tileset fix, hero tweaks

---
+++ Levels/level_01/walls_and_ground.gd
@@ -0,0 +1,9 @@
+extends TileMapLayer
+
+#func _use_tile_data_runtime_update(coords: Vector2i) -> bool:
+	#if coords in get_used_cells_by_id(1):
+		#return true
+	#return false
+	#
+#func _time_data_runtime_update(layer, coords, tile_data):
+	#tile_data.set_navigation_polygon(0,null)
