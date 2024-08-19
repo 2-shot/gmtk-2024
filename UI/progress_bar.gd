321e28ba17e599c18d1310b1ec0806124110e72a
Brandon Marshall <kem5brandon@gmail.com>
2024-08-19 12:53:48 -0700

    Update rats and hunger bar

--- UI/progress_bar.gd
+++ UI/progress_bar.gd
@@ -1,10 +1,19 @@
 extends ProgressBar
 
+# Get a local copy of your fill StyleBoxFlat
+@onready var progress_style : StyleBoxFlat = get_theme_stylebox("fill").duplicate()
+
 func size_changed(size : int):
 	value = size
+	var percent = (value - min_value) / (max_value - min_value)
+	modulate = Color.RED.lerp(Color.GREEN, percent)
+
+	GlobalSignals.debug.emit("Hunger percent", str(percent) + str(modulate))
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var percent = (value - min_value) / (max_value - min_value) 
+	modulate = Color.RED.lerp(Color.GREEN, percent)
 	GlobalSignals.hero_size.connect(size_changed)
 
 
